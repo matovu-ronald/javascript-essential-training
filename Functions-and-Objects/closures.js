72e66466cf80933090b0a1efbdfddf7ace6bc99c
Matovu Ronald <maronwhite6@gmail.com>
2019-06-15 12:58:31 +0300

    Closures

---
+++ Functions-and-Objects/closures.js
@@ -0,0 +1,16 @@
+// Closure is a function within another function that relies on the variables of the outside function
+// function addNumbers() {
+//     var a = 5;
+//     var b = 7;
+//     var sum = a + b;
+
+//     function multiply() {
+//         var result = a * b;
+//         return result;
+//     }
+
+//     return multiply;
+// }
+
+// var theResult = addNumbers();
+// console.log("The result is ", theResult());
