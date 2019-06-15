288b71c0f55075bb186538d555464027012c753e
Matovu Ronald <maronwhite6@gmail.com>
2019-06-15 11:08:18 +0300

    Arrays

---
+++ Working-with-data/arrays.js
@@ -0,0 +1,11 @@
+var pens;
+
+pens = ["red", "blue", "green", "orange"];
+
+console.log("Before: ", pens);
+
+// Properties
+// Get property of an object by name
+console.log("Array Length", pens.length);
+
+console.log("After: ", pens);
